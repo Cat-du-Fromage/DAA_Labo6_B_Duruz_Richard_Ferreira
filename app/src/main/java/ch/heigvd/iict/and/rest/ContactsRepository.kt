c00ad66f20791e47bd0d4954d1c61e0d2ee2c1a7
Richard Aurélien <aurelien.r@hotmail.ch>
2025-12-26 18:00:43 +0100

    DAO working ?

--- app/src/main/java/ch/heigvd/iict/and/rest/ContactsRepository.kt
+++ app/src/main/java/ch/heigvd/iict/and/rest/ContactsRepository.kt
@@ -1,9 +1,13 @@
 package ch.heigvd.iict.and.rest
 
 import ch.heigvd.iict.and.rest.database.ContactsDao
+import ch.heigvd.iict.and.rest.models.SyncState
 
 class ContactsRepository(private val contactsDao: ContactsDao) {
 
-    val allContacts = contactsDao.getAllContacts()
+    // allContacts should not take all all (don't take DELETED contacts) so we use getContacts instead
+    //val allContacts = contactsDao.getAllContacts()
+    val allContacts = contactsDao.getContacts()
+
 
 }
--- app/src/main/java/ch/heigvd/iict/and/rest/database/ContactsDao.kt
+++ app/src/main/java/ch/heigvd/iict/and/rest/database/ContactsDao.kt
@@ -6,6 +6,7 @@ import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
 import ch.heigvd.iict.and.rest.models.Contact
+import ch.heigvd.iict.and.rest.models.SyncState
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -20,14 +21,34 @@ interface ContactsDao {
     @Delete
     fun delete(contact: Contact)
 
+    // TODO : Probably irrelevant now because of the DELETED state. might remove
     @Query("SELECT * FROM Contact")
     fun getAllContacts() : Flow<List<Contact>>
+
+    // Only get contacts in specific states (by default synced, created and updated (omitting DELETED))
+    // TODO : if we go with this then we should update the getCount() too
+    @Query("SELECT * FROM Contact WHERE syncState IN (:states)")
+    fun getContacts(vararg states: SyncState = arrayOf(SyncState.SYNCED, SyncState.CREATED, SyncState.UPDATED)) : Flow<List<Contact>>
 
     @Query("SELECT * FROM Contact WHERE id = :id")
     fun getContactById(id : Long) : Contact?
 
-    @Query("SELECT COUNT(*) FROM Contact")
-    fun getCount() : Int
+    @Query("SELECT COUNT(*) FROM Contact WHERE syncState IN (:states)")
+    abstract fun countByStatus(states: List<SyncState>): Int
+
+    fun getCount(
+        states: List<SyncState> = listOf(SyncState.SYNCED, SyncState.CREATED, SyncState.UPDATED)
+    ) : Int {
+        return countByStatus(states)
+    }
+
+    // Could also be done like this ? tested and it works
+    // We will need to decide
+    // TODO : decide. This form has been used for getContacts
+    /*
+    @Query("SELECT COUNT(*) FROM Contact WHERE syncState IN (:states)")
+    fun getCount( vararg states: SyncState = arrayOf(SyncState.SYNCED, SyncState.CREATED, SyncState.UPDATED)) : Int
+    */
 
     @Query("DELETE FROM Contact")
     fun clearAllContacts()
